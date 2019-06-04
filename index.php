a7d86f410ad8d6dd88ac3ef89d6bea0e0f89d8ce
Birk Høgh-Mogensen <birk0169@edu.campusvejle.dk>
2019-06-04 09:19:13 +0200

    Added basic inferstructure, finished index, and adding styling

--- index.php
+++ index.php
@@ -17,16 +17,78 @@
         <link rel="stylesheet" href="css/styles.css" />
     </head>
     <body>
+    <div class="container">
         <!-- HEADER -->
         <header>
+            <nav class="navbar navbar-toggleable-sm navbar-expand-md navbar-inverse navbar-dark">
+                <div class="container">
+                    <img class="logo img-fluid navbar-left" src="img/temp-logo-2.png" alt="">
+                    <button class="navbar-toggler ml-auto hidden-sm-up float-xs-right" data-toggle="collapse" data-target="#mainNav">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="mainNav">
+                        
+                        <div class="navbar-nav">
+                            <a href="#" class="nav-item nav-link active">Home</a>
+                            <a href="#" class="nav-item nav-link">Shop</a>
+                            <a href="#" class="nav-item nav-link">Contact</a>
+                            <a href="#" class="nav-item nav-link mr-auto">Login</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <!-- Header Image -->
+            <div class="row">
+                <div class="col px-0">
+                    <img class="header-image img-fluid px-0" src="img\header-image-3.jpg" alt="">
+                </div>
+            </div>
         </header>
         <!-- MAIN -->
-                <!-- Articles -->
-
-        <!-- FOOTER -->
-        <footer>
+        <main>
+            <div class="row background-black">
+                <div class="col-lg-3 col-md-5">
+                    <h2>BIG Burger</h2>
+                </div>
+                <div class="col-lg-9 col-md-7">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet voluptatem officiis magnam ipsam odio officia cupiditate laborum cumque pariatur odit assumenda, aut nesciunt reiciendis ut, illum beatae, explicabo velit accusamus?</p>
+                </div>
+            </div>
+            <!-- Articles -->
             
-        </footer>
+            <div class="row">
+                <article class="col-md-4">
+                    <img class="img-fluid" src="img/burger-1.png" alt="">
+                    <div>
+                        <h3>Big Burger</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptatibus distinctio expedita quod porro consectetur? Quia error autem saepe ex ipsum officiis corrupti minima, blanditiis, debitis accusamus animi culpa veritatis!</p>
+                        <button class="btn mt-auto">Buy</button>
+                    </div>
+                </article>
+                <article class="col-md-4">
+                    <img class="img-fluid" src="img/burger-2.png" alt="">
+                    <div>
+                        <h3>Bigger Burger</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptatibus distinctio expedita quod porro consectetur? Quia error autem saepe ex ipsum officiis corrupti minima, blanditiis, debitis accusamus animi culpa veritatis!</p>
+                        <button class="btn mt-auto">Buy</button>
+                    </div>
+                </article>
+                <article class="col-md-4">
+                    <img class="img-fluid" src="img/burger-3.png" alt="">
+                    <div>
+                        <h3>Big Shroom Burger</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptatibus distinctio expedita quod porro consectetur? Quia error autem saepe ex ipsum officiis corrupti minima, blanditiis, debitis accusamus animi culpa veritatis!</p>
+                        <button class="btn mt-auto">Buy</button>
+                    </div>
+                </article>
+            </div>
+        </main>
+    </div>
+    <!-- FOOTER -->
+    <footer>
+        <h6>Adress: something 5b city</h6>
+    </footer>
+        
 
         <!-- JAVASCRIPT -->
         <!-- Bootstrap js -->
